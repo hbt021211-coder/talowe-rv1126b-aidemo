9c0547acf67fcc9dd0a6f0ca1de27abd600454b0
hbtao <hbtao1211@163..com>
2026-05-06 15:24:39 +0800

    add talowe_yolov8_garbage_sort_cam demo

---
+++ talowe_yolov8_garbage_sort_cam/cpp/build/build_rknn_yolov8_cam_rv1126b_linux_aarch64_Release/CMakeFiles/rknn_yolov8_cam.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/talowe/rv1126b_aidemo/yolov8/cpp/main.cc" "CMakeFiles/rknn_yolov8_cam.dir/main.cc.o" "gcc" "CMakeFiles/rknn_yolov8_cam.dir/main.cc.o.d"
+  "/home/talowe/rv1126b_aidemo/yolov8/cpp/postprocess.cc" "CMakeFiles/rknn_yolov8_cam.dir/postprocess.cc.o" "gcc" "CMakeFiles/rknn_yolov8_cam.dir/postprocess.cc.o.d"
+  "/home/talowe/rv1126b_aidemo/yolov8/cpp/rknpu2/yolov8.cc" "CMakeFiles/rknn_yolov8_cam.dir/rknpu2/yolov8.cc.o" "gcc" "CMakeFiles/rknn_yolov8_cam.dir/rknpu2/yolov8.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/talowe/rv1126b_aidemo/yolov8/cpp/build/build_rknn_yolov8_cam_rv1126b_linux_aarch64_Release/utils.out/CMakeFiles/imageutils.dir/DependInfo.cmake"
+  "/home/talowe/rv1126b_aidemo/yolov8/cpp/build/build_rknn_yolov8_cam_rv1126b_linux_aarch64_Release/utils.out/CMakeFiles/fileutils.dir/DependInfo.cmake"
+  "/home/talowe/rv1126b_aidemo/yolov8/cpp/build/build_rknn_yolov8_cam_rv1126b_linux_aarch64_Release/utils.out/CMakeFiles/imagedrawing.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ talowe_yolov8_garbage_sort_cam/cpp/build/build_rknn_yolov8_garbage_sort_cam_rv1126b_linux_aarch64_Release/CMakeFiles/rknn_yolov8_garbage_sort_cam.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/talowe/rv1126b_aidemo/talowe_yolov8_garbage_sort_cam/cpp/main.cc" "CMakeFiles/rknn_yolov8_garbage_sort_cam.dir/main.cc.o" "gcc" "CMakeFiles/rknn_yolov8_garbage_sort_cam.dir/main.cc.o.d"
+  "/home/talowe/rv1126b_aidemo/talowe_yolov8_garbage_sort_cam/cpp/postprocess.cc" "CMakeFiles/rknn_yolov8_garbage_sort_cam.dir/postprocess.cc.o" "gcc" "CMakeFiles/rknn_yolov8_garbage_sort_cam.dir/postprocess.cc.o.d"
+  "/home/talowe/rv1126b_aidemo/talowe_yolov8_garbage_sort_cam/cpp/rknpu2/yolov8.cc" "CMakeFiles/rknn_yolov8_garbage_sort_cam.dir/rknpu2/yolov8.cc.o" "gcc" "CMakeFiles/rknn_yolov8_garbage_sort_cam.dir/rknpu2/yolov8.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/talowe/rv1126b_aidemo/talowe_yolov8_garbage_sort_cam/cpp/build/build_rknn_yolov8_garbage_sort_cam_rv1126b_linux_aarch64_Release/utils.out/CMakeFiles/imageutils.dir/DependInfo.cmake"
+  "/home/talowe/rv1126b_aidemo/talowe_yolov8_garbage_sort_cam/cpp/build/build_rknn_yolov8_garbage_sort_cam_rv1126b_linux_aarch64_Release/utils.out/CMakeFiles/fileutils.dir/DependInfo.cmake"
+  "/home/talowe/rv1126b_aidemo/talowe_yolov8_garbage_sort_cam/cpp/build/build_rknn_yolov8_garbage_sort_cam_rv1126b_linux_aarch64_Release/utils.out/CMakeFiles/imagedrawing.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
